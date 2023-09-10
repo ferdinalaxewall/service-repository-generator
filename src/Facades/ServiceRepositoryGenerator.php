b3f624b9717a71fe72df3096de8663dbe17072f2
dayCod <alayarahman07@gmail.com>
2023-09-10 10:46:35 +0700

    Docs: Define some Function Inside Facades Folder

--- src/Facades/ServiceRepositoryGenerator.php
+++ src/Facades/ServiceRepositoryGenerator.php
@@ -11,7 +11,7 @@ class ServiceRepositoryGenerator extends Facade
      *
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'service-repository-generator';
     }
