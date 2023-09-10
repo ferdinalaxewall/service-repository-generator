545d10016293e9d4e0df473509f72db73f975451
dayCod <alayarahman07@gmail.com>
2023-09-10 11:27:33 +0700

    Docs: Text Comment ConsoleLog.php & All Stubs

--- src/Helpers/ConsoleLog.php
+++ src/Helpers/ConsoleLog.php
@@ -4,13 +4,27 @@ namespace Ferdinalaxewall\ServiceRepositoryGenerator\Helpers;
 
 class ConsoleLog
 {
-    public static function info($message)
+    /**
+     * create static function for generate the info console command.
+     *
+     * @param string $message
+     *
+     * @return void
+     */
+    public static function info(string $message): void
     {
         $output = new \Symfony\Component\Console\Output\ConsoleOutput();
         $output->writeln("<info>{$message}</info>");
     }
 
-    public static function error($message)
+    /**
+     * create static function for generate the error console command.
+     *
+     * @param string $message
+     *
+     * @return void
+     */
+    public static function error(string $message): void
     {
         $output = new \Symfony\Component\Console\Output\ConsoleOutput();
         $output->writeln("<error>{$message}</error>");
