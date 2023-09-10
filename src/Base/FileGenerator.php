8f7625cc8d3ce3f41232d1b23e4f0ca28e1a2c8c
dayCod <alayarahman07@gmail.com>
2023-09-10 10:32:20 +0700

    Docs: Text Comment FileGenerator.php

--- src/Base/FileGenerator.php
+++ src/Base/FileGenerator.php
@@ -7,33 +7,80 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Filesystem\Filesystem;
 use Ferdinalaxewall\ServiceRepositoryGenerator\Helpers\ConsoleLog;
 use Ferdinalaxewall\ServiceRepositoryGenerator\Exceptions\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Collection;
 
 abstract class FileGenerator
 {
+    /**
+     * create an initial variable for the file system facades.
+     *
+     * @var \Illuminate\Filesystem\Filesystem $files
+     */
     protected $files;
+
+    /**
+     * create an initial variable for the name of model.
+     *
+     * @var \Illuminate\Database\Eloquent\Model $modelName
+     */
     protected $modelName;
+
+    /**
+     * create an initial variable for check the truthy or falsy condition.
+     *
+     * @var Boolean
+     */
     protected $isImplementClass;
 
+    /**
+     * create a new FileGenerator instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->files = new Filesystem;
     }
 
+    /**
+     * abstraction class for getting the path of stubs.
+     *
+     * @return string
+     */
     abstract public function getStubPath(): string;
+
+    /**
+     * abstraction class for getting the variables of stubs.
+     *
+     * @return array
+     */
     abstract public function getStubVariables(): array;
+
+    /**
+     * abstraction class for getting the file source path.
+     *
+     * @return string
+     */
     abstract public function getSourceFilePath(): string;
 
-    public function generate(string $modelName)
+    /**
+     * create a base function to generate class files.
+     *
+     * @param string $modelName
+     *
+     * @return void
+     */
+    public function generate(string $modelName): void
     {
         if(file_exists(app_path('Models/'.$modelName.'.php'))){
             $this->modelName = $modelName;
-    
+
             $baseServiceFilePath = $this->getSourceFilePath();
             $this->makeDirectory(dirname($baseServiceFilePath));
-    
+
             // Create Interface File
             $this->createFile($baseServiceFilePath, $this->getSourceFileContent());
-    
+
             // Create Implement Class File
             $this->createFile($this->getSourceFilePath(true), $this->getSourceFileContent(true));
         }else{
@@ -41,7 +88,15 @@ abstract class FileGenerator
         }
     }
 
-    public function createFile($filePath, $fileContents)
+    /**
+     * create a base function for creating files.
+     *
+     * @param string $filePath
+     * @param mixed $fileContents
+     *
+     * @return void
+     */
+    public function createFile(string $filePath, mixed $fileContents): void
     {
         if (!$this->files->exists($filePath)) {
             $this->files->put($filePath, $fileContents);
@@ -51,13 +106,29 @@ abstract class FileGenerator
         }
     }
 
-    public function getSourceFileContent($isImplementClass = false)
+    /**
+     * create a base function for getting the source of file content.
+     *
+     * @param bool $isImplementClass
+     *
+     * @return string|false
+     */
+    public function getSourceFileContent(bool $isImplementClass = false): string|false
     {
         $this->isImplementClass = $isImplementClass;
+
         return $this->getStubContents($this->getStubPath(), $this->getStubVariables());
     }
 
-    public function getStubContents($stub , $stubVariables = [])
+    /**
+     * create a base function for getting the contents of stubs.
+     *
+     * @param mixed $stub
+     * @param array $stubVariables
+     *
+     * @return string|false
+     */
+    public function getStubContents(string|false $stub , array $stubVariables = []): string|false
     {
         $contents = file_get_contents($stub);
 
@@ -70,14 +141,29 @@ abstract class FileGenerator
 
     }
 
-    public function getSingularClassName($name)
+    /**
+     * create a base function for getting the singular class name.
+     *
+     * @param string $name
+     *
+     * @return string
+     */
+    public function getSingularClassName(string $name): string
     {
         return ucwords(Pluralizer::singular($name));
     }
 
-    protected function makeDirectory($path)
+    /**
+     * make the directory with allow entire permission 0777. include admin, user and guest.
+     *
+     * @param mixed $path
+     *
+     * @return string|Collection|array
+     */
+    protected function makeDirectory(mixed $path): string|Collection|array
     {
         if (! $this->files->isDirectory($path)) $this->files->makeDirectory($path, 0777, true, true);
+
         return $path;
     }
 }
