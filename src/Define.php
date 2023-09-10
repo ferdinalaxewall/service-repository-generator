a7d989e779ef8323d882fbd8653096e9c5eb16df
dayCod <alayarahman07@gmail.com>
2023-09-10 11:32:47 +0700

    Docs: Text Comment Define.php

--- src/Define.php
+++ src/Define.php
@@ -4,14 +4,45 @@ namespace Ferdinalaxewall\ServiceRepositoryGenerator;
 
 class Define
 {
+    /**
+     * give the value for available type of generators.
+     *
+     * @var array<string>
+     */
     CONST AVAILABLE_TYPE = ['service', 'repository'];
+
+    /**
+     * give the value for base repository file path.
+     *
+     * @var string
+     */
     CONST BASE_REPOSITORY_FILE_PATH = '/BaseRepository.php';
 
-    // Service Definition
+    /**
+     * give the value for service type definition.
+     *
+     * @var string
+     */
     CONST SERVICE_TYPE = 'service';
+
+    /**
+     * give the value for service path definition.
+     *
+     * @var string
+     */
     CONST SERVICE_PATH = 'app/Services';
-    
-    // Repository Definition
+
+    /**
+     * give the value for repository type definition.
+     *
+     * @var string
+     */
     CONST REPOSITORY_TYPE = 'repository';
+
+    /**
+     * give the value for repository path definition.
+     *
+     * @var string
+     */
     CONST REPOSITORY_PATH = 'app/Repositories';
 }
